4ac1e679ce9ac1530cbad012c8863fd95e903711
Md Hashmi <mdhashmi0711@gmail.com>
2025-09-24 10:32:32 +0530

    Create Navbar, Routing, Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Body from "./components/Body"
+import Login from "./components/Login"
+import Profile from "./components/profile"
 
 function App() {
 
   return (
     <>
-      
-      <h1>Hello World</h1>
-      
+
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/test" element={<div>Test Page</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
